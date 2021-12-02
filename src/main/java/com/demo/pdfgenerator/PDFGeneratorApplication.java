b380565c72a7343153cc6b09f9c24c2c27979a58
atwoodreed1Git <82432559+atwoodreed1Git@users.noreply.github.com>
2021-12-02 11:09:24 -0700

    Initial Commit
    Created Spring project for testing PDF libraries from the spring initializer at https://start.spring.io/

---
+++ src/main/java/com/demo/pdfgenerator/PDFGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.demo.pdfgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PDFGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PDFGeneratorApplication.class, args);
+	}
+
+}
