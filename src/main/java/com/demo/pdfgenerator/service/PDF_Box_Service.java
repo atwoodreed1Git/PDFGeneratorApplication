078279ba9e6991637b3a14b3a3e34bcd524dc269
atwoodreed1Git <82432559+atwoodreed1Git@users.noreply.github.com>
2021-12-09 08:48:46 -0700

    Added comment about PDF Generation for when working on this again

--- src/main/java/com/demo/pdfgenerator/service/PDF_Box_Service.java
+++ src/main/java/com/demo/pdfgenerator/service/PDF_Box_Service.java
@@ -2,6 +2,19 @@ package com.demo.pdfgenerator.service;
 
 import org.springframework.stereotype.Service;
 
+/**
+ PDF rendering
+
+ I'm getting an OutOfMemoryError. What can I do?
+ The memory footprint depends on the PDF itself and on the resolution you use for rendering. Some possible options:
+
+ increase the -Xmx value when starting java
+ use a scratch file by loading files with this code PDDocument.load(file, MemoryUsageSetting.setupTempFileOnly())
+ be careful not to hold your images after rendering them, e.g. avoid putting all images of a PDF into a List
+ don't forgot to close your PDDocument objects
+ decrease the scale when calling PDFRenderer.renderImage(), or the dpi value when calling PDFRenderer.renderImageWithDPI()
+ disable the cache for PDImageXObject objects by calling PDDocument.setResourceCache() with a cache object that is derived from DefaultResourceCache and whose call public void put(COSObject indirect, PDXObject xobject) does nothing. Be aware that this will slow down rendering for PDF files that have an identical image in several pages (e.g. a company logo or a background). More about this can be read in PDFBOX-3700.
+ ***/
 @Service
 public class PDF_Box_Service {
 
